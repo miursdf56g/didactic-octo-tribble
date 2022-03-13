ef3d6ecd339f73cb0ef2e0498ae8ef7cec98d0ba
miursdf56g <97636503+miursdf56g@users.noreply.github.com>
2022-03-13 04:00:27 -0500

    Update installnvim.sh

--- installnvim.sh
+++ installnvim.sh
@@ -5,11 +5,8 @@ mkdir pack
 mkdir pack/vendor
 mkdir pack/vendor/start
 cd pack/vendor/start
-git clone https://github.com/preservim/nerdtree
-git clone https://github.com/srcery-colors/srcery-vim
 git clone https://github.com/vim-syntastic/syntastic
-git clone https://github.com/cww/vim-easyeyes
-git clone https://github.com/ghifarit53/tokyonight-vim
+git clone https://github.com/ayu-theme/ayu-vim
 git clone https://github.com/vimwiki/vimwiki
 cd ~/Downloads/dotfiles/
 mv init.vim ~/.config/nvim
